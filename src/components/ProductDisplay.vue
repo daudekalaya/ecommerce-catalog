8cf03c4c75c3c26072247546200aa20005cfb373
daudekalaya <daud.eklya31@gmail.com>
2023-03-30 06:17:38 +0700

    first commit

---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,205 @@
+<template>
+  <div v-if="isLoading" class="card">
+    <div class="contain-product">
+      <div class="skeleton">
+        <div class="skeleton-left flex1">
+          <div class="square"></div>
+        </div>
+        <div class="skeleton-right flex2">
+          <div class="line"></div>
+          <div class="line h250"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div
+    v-else
+    class="container"
+    :class="
+      !isProductAvailable
+        ? 'bg-gray'
+        : item.data.category === 'men\'s clothing'
+        ? 'bg-blue'
+        : 'bg-pink'
+    "
+  >
+    <img class="patern" src="../assets/bg-pattern.svg" alt="" />
+    <div class="contain-product">
+      <div v-if="!isProductAvailable" class="product-unavailable-container">
+        <div class="overlay">
+          <img
+            src="../assets/sad-face.svg"
+            alt="unavailable product"
+            srcset=""
+          />
+        </div>
+        <div class="content">
+          <p>This product is unavailable to show</p>
+          <div class="btn-available">
+            <button
+              type="button"
+              @click="nextItem()"
+              class="btn-available-next"
+            >
+              Next Product
+            </button>
+          </div>
+        </div>
+      </div>
+      <div v-else>
+        <div class="img-product">
+          <img :src="item.data.image" :alt="item.data.title" />
+        </div>
+        <div class="content">
+          <div class="wrapper">
+            <h3
+              :class="
+                item.data.category === 'men\'s clothing'
+                  ? 'font-navy'
+                  : 'font-magenta'
+              "
+              class="title"
+            >
+              {{ item.data.title }}
+            </h3>
+            <div class="sub-title">
+              <span>{{ item.data.category }}</span>
+              <div class="rating">
+                <span>{{ item.data.rating.rate }}/5</span>
+                <div class="rating">
+                  <span
+                    :class="
+                      item.data.category === 'men\'s clothing'
+                        ? 'bg-navy'
+                        : 'bg-magenta'
+                    "
+                    class="circle"
+                  ></span>
+                  <span
+                    :class="
+                      item.data.category === 'men\'s clothing'
+                        ? 'bg-navy'
+                        : 'bg-magenta'
+                    "
+                    class="circle"
+                  ></span>
+                  <span
+                    :class="
+                      item.data.category === 'men\'s clothing'
+                        ? 'bg-navy'
+                        : 'bg-magenta'
+                    "
+                    class="circle"
+                  ></span>
+                  <span
+                    :class="
+                      item.data.category === 'men\'s clothing'
+                        ? 'bg-navy'
+                        : 'bg-magenta'
+                    "
+                    class="circle"
+                  ></span>
+                  <span
+                    :class="
+                      item.data.category === 'men\'s clothing'
+                        ? 'bg-navy'
+                        : 'bg-magenta'
+                    "
+                    class="circle"
+                  ></span>
+                </div>
+              </div>
+            </div>
+            <p class="desc">
+              {{ item.data.description }}
+            </p>
+          </div>
+          <div class="wrapper2">
+            <p
+              :class="
+                item.data.category === 'men\'s clothing'
+                  ? 'font-navy'
+                  : 'font-magenta'
+              "
+              class="price"
+            >
+              ${{ item.data.price }}
+            </p>
+            <div class="btn">
+              <button
+                :class="
+                  item.data.category === 'men\'s clothing'
+                    ? 'bg-navy'
+                    : 'bg-magenta'
+                "
+                class="btn-buy"
+              >
+                Buy now
+              </button>
+              <button
+                @click="nextItem()"
+                :class="
+                  item.data.category === 'men\'s clothing'
+                    ? 'border-navy font-navy'
+                    : 'border-magenta font-magenta'
+                "
+                class="btn-next"
+              >
+                Next product
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductDisplay",
+  data() {
+    return {
+      isLoading: false,
+      index: 0,
+      isProductAvailable: false,
+      item: [],
+    };
+  },
+  methods: {
+    async fetchData() {
+      const response = await fetch(
+        `https://fakestoreapi.com/products/${this.index}`
+      );
+      const result = await response.json();
+      return result;
+    },
+    async nextItem() {
+      this.isLoading = true;
+
+      if (this.index !== 20) {
+        this.index++;
+      } else {
+        this.index = 1;
+      }
+
+      let data = await this.fetchData();
+      console.log(data);
+      if (
+        data.category === "men's clothing" ||
+        data.category === "women's clothing"
+      ) {
+        this.item = { data };
+        this.isProductAvailable = true;
+      } else {
+        this.isProductAvailable = false;
+      }
+
+      this.isLoading = false;
+    },
+  },
+  mounted() {
+    this.nextItem();
+  },
+};
+</script>
